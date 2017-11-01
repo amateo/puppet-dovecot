edc6dfc9802dcbe4f07c99a223db2564d6698666
Justin Couch <jcouch@choice.com.au>
2017-11-02 09:44:02 +1100

    Redhat-specific family package names - most of which involves ignoring installation.

--- manifests/ldap/install.pp
+++ manifests/ldap/install.pp
@@ -1,8 +1,15 @@
 #
 class dovecot::ldap::install inherits dovecot::ldap {
-  if $dovecot::package_manage {
-    package {$dovecot::ldap::package_name:
-      ensure => $dovecot::package_ensure,
+  case $facts['os']['family'] {
+    'Debian': {
+      if $dovecot::package_manage {
+        package {$dovecot::ldap::package_name:
+          ensure => $dovecot::package_ensure,
+        }
+      }
+    }
+    'RedHat': {
+      # Do nothing since this is included in the core yum dovecot package
     }
   }
 }
--- manifests/pop3/install.pp
+++ manifests/pop3/install.pp
@@ -1,8 +1,15 @@
 #
 class dovecot::pop3::install inherits dovecot::pop3 {
-  if $dovecot::package_manage {
-    package {$dovecot::pop3::package_name:
-      ensure => $dovecot::package_ensure,
+  case $facts['os']['family'] {
+    'Debian': {
+      if $dovecot::package_manage {
+        package {$dovecot::pop3::package_name:
+          ensure => $dovecot::package_ensure,
+        }
+      }
+    }
+    'RedHat': {
+      # Do nothing since this is included in the core yum dovecot package
     }
   }
 }
